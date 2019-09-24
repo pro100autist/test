3a694cc5baaf3e3225202c53be2d9c8e0c8aeed5
pro100autist <55575350+pro100autist@users.noreply.github.com>
2019-09-25 00:40:56 +0300

    Добавьте файлы проекта.

---
+++ bot/Controllers/CallbackController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using VkNet.Model;
+using VkNet.Utils;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.Model.RequestParams;
+
+namespace bot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        /// <summary>
+        /// Конфигурация приложения
+        /// </summary>
+        private readonly IConfiguration _configuration;
+        private readonly IVkApi _vkApi;
+
+
+        public CallbackController(IVkApi vkApi, IConfiguration configuration)
+        {
+            _vkApi = vkApi;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Callback([FromBody] Updates updates)
+        {
+            // Тип события
+            switch (updates.Type)
+            {
+                // Ключ-подтверждение
+                case "confirmation":
+                    {
+                        return Ok(_configuration["Config:Confirmation"]);
+                    }
+
+                // Новое сообщение
+                case "message_new":
+                    {
+                        // Десериализация
+                        var msg = Message.FromJson(new VkResponse(updates.Object));
+                        _vkApi.Messages.Send(new MessagesSendParams
+                        {
+                            RandomId = new DateTime().Millisecond,
+                            PeerId = msg.PeerId.Value,
+                            Message = msg.Text
+                        });
+                        break;
+                    }
+            }
+
+            return Ok("ok");
+        }
+    }
+}
